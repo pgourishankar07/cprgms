db0135995bb5303855c240b8d5cd2eac49c03c36
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-01-08 10:58:43 +0530

    function overidding

--- Apna college/ All DSA/Cls&Objs/5.cpp
+++ Apna college/ All DSA/Cls&Objs/5.cpp
@@ -9,6 +9,47 @@ class triangle{
     public:
         int br;
         int ht;
+        float area(){
+            return 0.5*br*ht;
+        }
+        
+};
+
+class rectangle : public triangle{
+    public:
+        rectangle(int b,int h){
+            br=b;
+            ht=h;
+        }
+        float area(){
+            return br*ht;
+        }
+        
+};
+
+
+int main() {
+    rectangle a(5,15);
+    // a.br = 5;
+    // a.ht = 15;
+    cout<<"Area of Rectangle : "<<a.area()<<endl;
+    cout<<"Area of triangle : "<<a.triangle::area()<<endl;
+    
+    return 0;
+}
+
+// ____________________________NORMAL METHOD uisng only object and classes without inheritance__________________________________
+
+/*
+
+#include <iostream>
+using namespace std;
+
+
+class triangle{
+    public:
+        int br;
+        int ht;
         int area(){
             return 0.5*br*ht;
         }
@@ -34,9 +75,11 @@ class rectangle {
 
 int main() {
     triangle a(5,10);
-    cout<<"Area of triangle : "<<a.area()<<endl;
     rectangle b(15,10);
+    cout<<"Area of triangle : "<<a.area()<<endl;
     cout<<"Area of Rectangle : "<<b.area()<<endl;
     
     return 0;
 }
+
+*/
--- Apna college/ All DSA/Cls&Objs/tempCodeRunnerFile.cpp
+++ Apna college/ All DSA/Cls&Objs/tempCodeRunnerFile.cpp
@@ -1,4 +1,42 @@
-rectangle(int b,int h){
-        //     br=b;
-        //     ht=h;
-        // }
+#include <iostream>
+// using namespace std;
+
+
+// class triangle{
+//     public:
+//         int br;
+//         int ht;
+//         int area(){
+//             return 0.5*br*ht;
+//         }
+//         // triangle(int b,int h){
+//         //     br=b;
+//         //     ht=h;
+//         // }
+// };
+
+// class rectangle : public triangle{
+//     public:
+//         // int br;
+//         // int ht;
+//         int area(){
+//             return br*ht;
+//         }
+//         // rectangle(int b,int h){
+//         //     br=b;
+//         //     ht=h;
+//         // }
+// };
+
+
+// int main() {
+//     // triangle a(5,10);
+//     rectangle a;
+//     a.br = 5;
+//     a.ht = 15;
+//     cout<<"Area of triangle : "<<a.area()<<endl;
+//     // rectangle b(15,10);
+//     cout<<"Area of Rectangle : "<<a.triangle::area()<<endl;
+    
+//     return 0;
+// }
