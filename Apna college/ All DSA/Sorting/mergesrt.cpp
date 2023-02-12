10740944c8c9c949c618f470adeb8abc3783361f
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-02-12 16:42:55 +0530

    mrg2

--- Apna college/ All DSA/Sorting/mergesrt.cpp
+++ Apna college/ All DSA/Sorting/mergesrt.cpp
@@ -4,25 +4,34 @@ int main() {
   int arr1[4], arr2[4];
   int arr3[8];
   for (int i = 0; i < 4; i++) {
-    cout << "Enter array 1 :" << endl;
+    cout << "Enter array 1 :";
     cin >> arr1[i];
-    cout << "Enter array 2 :" << endl;
+
+    cout << "Enter array 2 :";
     cin >> arr2[i];
   }
 
+  int ptr1 = 0, ptr2 = 0;
   for (int i = 0; i < 8; i++) {
-    int ptr1 = 0, ptr2 = 0;
     if (arr1[ptr1] < arr2[ptr2]) {
       arr3[i] = arr1[ptr1];
-      ptr1++;
-    } else {
+      ptr1 = ptr1 + 1;
+    } else if (i == 7) {
+      if (arr1[3] < arr2[3]) {
+        arr3[i] = arr2[3];
+      } else {
+        arr3[i] = arr1[3];
+      }
+    }
+
+    else {
       arr3[i] = arr2[ptr2];
-      ptr2++;
+      ptr2 = ptr2 + 1;
     }
   }
-  cout << "Output";
+  cout << "Output : ";
   for (int i = 0; i < 8; i++) {
-    cout << arr3[i] << endl;
+    cout << arr3[i] << " ";
   }
 
   return 0;
