5678200912eb413541a4016300d92b7eed32b86d
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-03-15 22:58:04 +0530

    merge 2

--- Apna college/ All DSA/Sorting/mergeSort.cpp
+++ Apna college/ All DSA/Sorting/mergeSort.cpp
@@ -1,3 +1,5 @@
+// _______________________with uisng recursion______________
+
 #include <iostream>
 using namespace std;
 
@@ -5,37 +7,36 @@ void merge(int arr[], int l, int mid, int r) {
   int n1 = mid - l + 1; // n1&n2 are sub_arr_size 1 and 2
   int n2 = r - mid;
 
-  int arr1[n1];
-  int arr2[n2];
+  int L[n1];
+  int R[n2];
 
   for (int i = 0; i < n1; i++) {
-    arr1[i] = arr[l + i];
+    L[i] = arr[l + i];
   }
   for (int i = 0; i < n2; i++) {
-    arr2[i] = arr[mid + 1 + i];
+    R[i] = arr[mid + 1 + i];
   }
 
-  int ptr1 = 0, ptr2 = 0, k = 0;
-  while (ptr1 < n1 && ptr2 < n2) {
-    if (arr1[ptr1] < arr2[ptr2]) {
-      arr[k] = arr1[ptr1];
-      k++;
-      ptr1++;
+  int i = 0, j = 0, k = 0;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
     } else {
-      arr[k] = arr2[ptr2];
-      k++;
-      ptr2++;
+      arr[k] = R[j];
+      j++;
     }
-  }
-  while (ptr1 < n1) {
-    arr[k] = arr1[ptr1];
     k++;
-    ptr1++;
   }
-  while (ptr2 < n2) {
-    arr[k] = arr2[ptr2];
+  while (i < n1) {
+    arr[k] = L[i];
     k++;
-    ptr2++;
+    i++;
+  }
+  while (j < n2) {
+    arr[k] = R[j];
+    k++;
+    j++;
   }
 }
 
@@ -50,13 +51,77 @@ void mergeSort(int arr[], int l, int r) {
 }
 
 int main() {
+  int arr[] = {12, 11, 13, 5, 6, 7};
+  int n = sizeof(arr) / sizeof(arr[0]);
 
-  int arr[] = {5,4,3,2,1};
-  mergeSort(arr, 0, 4);
+  mergeSort(arr, 0, n);
 
-  for (int i = 0; i < 5; i++) {
+  cout << "Sorted array is: \n";
+  for (int i = 0; i < n; i++)
     cout << arr[i] << " ";
-  }
 
   return 0;
 }
+
+// without recursion____________
+
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
+
+// void merge(int arr[], int l, int mid, int r) {
+//   int n1 = mid - l + 1;
+//   int n2 = r - mid;
+//   int L[n1], R[n2];
+
+//   for (int i = 0; i < n1; i++)
+//     L[i] = arr[l + i];
+//   for (int i = 0; i < n2; i++)
+//     R[i] = arr[mid + 1 + i];
+
+//   int i = 0, j = 0, k = l;
+//   while (i < n1 && j < n2) {
+//     if (L[i] <= R[j]) {
+//       arr[k] = L[i];
+//       i++;
+//     } else {
+//       arr[k] = R[j];
+//       j++;
+//     }
+//     k++;
+//   }
+//   while (i < n1) {
+//     arr[k] = L[i];
+//     i++;
+//     k++;
+//   }
+//   while (j < n2) {
+//     arr[k] = R[j];
+//     j++;
+//     k++;
+//   }
+// }
+
+// void mergeSort(int arr[], int n) {
+//   int curr_size, l;
+//   for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
+//     for (l = 0; l < n - 1; l += 2 * curr_size) {
+//       int mid = min(l + curr_size - 1, n - 1);
+//       int r = min(l + 2 * curr_size - 1, n - 1);
+//       merge(arr, l, mid, r);
+//     }
+//   }
+// }
+
+// int main() {
+//   int arr[] = {12, 11, 13, 5, 6, 7};
+//   int n = sizeof(arr) / sizeof(arr[0]);
+
+//   mergeSort(arr, n);
+
+//   cout << "Sorted array is: \n";
+//   for (int i = 0; i < n; i++)
+//     cout << arr[i] << " ";
+
+//   return 0;
+// }
