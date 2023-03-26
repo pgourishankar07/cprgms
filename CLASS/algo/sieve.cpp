7d3df30c3c4cdc0da56359acb7fd4a961014fe1a
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-03-26 15:59:24 +0530

    SieveOfEratosthenes

--- CLASS/algo/sieve.cpp
+++ CLASS/algo/sieve.cpp
@@ -1,6 +1,32 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 int main() {
-
-  return 0;
+    int n;
+    cout << "Enter number of series u want : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+    }
+    for (int i = 2; i < sqrt(n); i++) {
+        int count = 0;
+        for (int j = i; j <= n; j++) {
+            if (arr[j - 1] % i == 0 && count == 0) {
+                count++;
+                continue;
+            }
+            if (arr[j - 1] % i == 0) {
+                arr[j - 1] = 0;
+            }
+        }
+    }
+    cout << "The Prime number Series are : " << endl;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1 || arr[i] == 0) {
+            continue;
+        }
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
--- CLASS/algo/tempCodeRunnerFile.cpp
+++ CLASS/algo/tempCodeRunnerFile.cpp
@@ -1,3 +1,14 @@
- euclidsAlgo(n1, n2);
-
-  //   cout
+for (int i = 2; i < n; i++) {
+        int count = 0;
+        for (int j = 1; j < n; j++) {
+            if (arr[j] % i == 0 && count == 0) {
+                count++;
+                continue;
+            }
+            if (arr[j] % i == 0) {
+                cout << endl
+                     << "______" << arr[j] << "______" << endl;
+                arr[j] = 0;
+            }
+        }
+    }
