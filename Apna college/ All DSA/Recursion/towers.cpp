dc4a898fc9f755e6eff1cdb058b676c6cf33a393
pgourishankar07 <100337318+pgourishankar07@users.noreply.github.com>
2023-04-01 13:03:52 +0530

    towerOfHAnoi

---
+++ Apna college/ All DSA/Recursion/towers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void towersOfHanoi(int n, string src, string help, string dest) {
+
+  if (n == 1) {
+
+    cout << "______________Disk transfer " << n << " from " << src << " to " << dest << endl;
+    return;
+  }
+
+  towersOfHanoi(n - 1, src, dest, help);
+  cout << "______________Disk transfer " << n << " from " << src << " to " << dest << endl;
+  towersOfHanoi(n - 1, help, src, dest);
+}
+
+int main() {
+  int n = 3;
+  towersOfHanoi(n, "S", "H", "D");
+  return 0;
+}
